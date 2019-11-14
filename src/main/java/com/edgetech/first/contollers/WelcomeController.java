96568a41e1df5967899b501c705962be40f7a84a
Eric Couch <james.couch@ruminatetech.com>
2019-11-13 20:04:00 -0600

    added complex model to controller

--- src/main/java/com/edgetech/first/contollers/WelcomeController.java
+++ src/main/java/com/edgetech/first/contollers/WelcomeController.java
@@ -5,10 +5,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private StudentService eric = new StudentService("Eric", 47);
     @RequestMapping("/welcome")
     public String welcome() {
-        String name = "Eric";
-        return "This is a string return from the server.  Hello " + name;
+        return "Student Name: " + eric.getName() + "<br>" + "Student age: " + eric.getAge();
+    }
+}
+
+class WelcomeService {
+    public String returnWelcomeMessage() {
+        return "Hello World!";
+    }
+}
+
+class StudentService {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public StudentService(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentService{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
